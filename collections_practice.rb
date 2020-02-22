884ac4d655993cc56ca4eadf9594456bbb8297c5
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 06:35:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+array = [25, 7, 1]
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a}
+end
+
+array.sort { |a, b| a.length <=> b.length }
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|i| i[2] = "$"}
+end
