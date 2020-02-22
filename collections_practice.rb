0fad1dc084375c35ca6fe59a99e05863138212e0
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 06:36:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,8 +9,8 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-array.sort { |a, b| a.length <=> b.length }
-rnd
+  array.sort { |a, b| a.length <=> b.length }
+end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
